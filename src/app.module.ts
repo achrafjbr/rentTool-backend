68c040fccfda1815e24937f3216c54ba6656ca08
achrafjbr <developerjbr@gmail.com>
2026-07-24 01:17:21 +0200

    Setup db & jwt configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      // imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          appName: configService.get<string>('APP_NAME'),
+          dbName: configService.get<string>('RENT_TOOL'),
+          uri: configService.get<string>('DB_URL'),
+        };
+      },
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          global: true,
+          secret: configService.get<string>('JWT_KEY'),
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
